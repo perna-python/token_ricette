94e6f388e10c54416996d83ecca40a31762227bc
Stefano Perna <stecoding@gmail.com>
2026-02-21 18:05:39 +0100

    feat: add example configuration for email and APSS credentials

---
+++ token_ricette/config_example.py
@@ -0,0 +1,11 @@
+email_config = {
+    "EMAIL_ADDRESS": "email",
+    "EMAIL_PASSWORD": "password",
+    "IMAP_SERVER": "imap server"
+}
+
+apss_config = {
+    "LOGIN_URL": "https://servizi.apss.tn.it/farmacie/login.php",
+    "USERNAME": "username apss",
+    "PASSWORD": "password apss"
+}
